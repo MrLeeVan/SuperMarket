0192246972e6606b801511cdd8fd4291f10e500f
leevan <llfshi520@qq.com>
2018-11-20 17:46:10 +0800

    最新

---
+++ src/main/test/ProductsXmlUtilsTest.java
@@ -0,0 +1,18 @@
+package main.test;
+import main.Bean.Clothes;
+import main.Utils.ProductsXmlUtils;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author Leevan
+ * @Date： 2018/11/20 14:34
+ */
+public class ProductsXmlUtilsTest{
+    @Test
+    public void test(){
+        List<Clothes> clothes = ProductsXmlUtils.parserProductFormXml();
+        System.out.println( Arrays.toString(clothes.toArray()));
+    }
+}
