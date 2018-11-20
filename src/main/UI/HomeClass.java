ed5d9a17f16804f9cff2be2b378c736c95d84dac
leevan <llfshi520@qq.com>
2018-11-20 19:56:58 +0800

    欢迎界面效果展示

--- src/main/UI/HomeClass.java
+++ src/main/UI/HomeClass.java
@@ -14,7 +14,46 @@ import java.util.List;
 public class HomeClass extends BaseClass {
     public void show(){
        showProducts();
+       println( "welcome:" +currUser.getUsername() );
+       boolean flag = true;
+       while(flag){
+           println( getString( "home.function" ) );
+           println( getString( "info.select" ) );
+           String select = input.nextLine();
+           switch (select){
+               case "1":/*查询全部订单*/
+                   findList();
+                   flag = false;
+                   break;
+               case "2":/*查找订单*/
+                   findOrderById();
+                   flag = false;
+                   break;
+               case "3":/*购买*/
+                   buyProducts();
+                   flag = false;
+                   break;
+               case "0":/*退出*/
+                   flag = false;
+                   System.exit( 0 );
+                   break;
+               default:
+                   println( getString( "input.error" ) );
+
+           }
+
+       }
     }
+
+    private void findList(){}
+
+    private void findOrderById(){}
+
+    private void buyProducts(){}
+
+
+
+
     private void showProducts(){
         ClothesService clothesService = new ClothesServiceImpl();
         List<Clothes> list = clothesService.list();
